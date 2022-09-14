f4bad60a08614a4747965377345d03e6a921b727
PratikSaha198 <pratiksaha198@gmail.com>
2022-09-14 13:35:51 +0530

    Added Routing, Route Guard and minor changes

--- smallchange/src/app/app-routing.module.ts
+++ smallchange/src/app/app-routing.module.ts
@@ -9,16 +9,23 @@ import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.co
 import { PreferencesPageComponent } from './pages/preferences-page/preferences-page.component';
 import { TradePageComponent } from './pages/trade-page/trade-page.component';
 import { RegPageComponent } from './pages/reg-page/reg-page.component'
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
+
   { path: '', component: LoginPageComponent },
   { path: 'registration', component: RegPageComponent },
-  { path: 'activity', component: ActivityPageComponent},
+
+  { path: 'activity', component: ActivityPageComponent, canActivate:[AuthGuard]},
   { path: 'home', component: HomePageComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'portfolio', component: PortfolioPageComponent },
-  { path: 'preference', component: PreferencesPageComponent },
-  { path: 'trade', component: TradePageComponent }
+
+  // { path: 'login', component: LoginPageComponent },
+  
+  { path: 'portfolio', component: PortfolioPageComponent, canActivate:[AuthGuard] },
+  
+  { path: 'preference', component: PreferencesPageComponent, canActivate:[AuthGuard] },
+  
+  { path: 'trade', component: TradePageComponent, canActivate:[AuthGuard] }
 ];
 
 @NgModule({
